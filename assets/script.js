b3506e82f8fd9ed5c5aa4c1c7dac6b798778a0d9
Francis Gonzales <fgonzalestello91@gmail.com>
2022-08-19 17:46:28 -0500

    Interval

--- assets/script.js
+++ assets/script.js
@@ -43,21 +43,24 @@ function snapshot() {
     });
 }
 
+let playCanvas;
+let playDisplaySize;
+
 webcam.addEventListener('play', () => {
-    let canvas = document.getElementById('snapshot-canvas')
+    let playCanvas = document.getElementById('snapshot-canvas')
     var marco = document.getElementById('marco');
 
-    if (!canvas) {
-        canvas = faceapi.createCanvasFromMedia(webcam);
-        canvas.setAttribute('id', 'snapshot-canvas')
-        webcamContent.append(canvas)
+    if (!playCanvas) {
+        playCanvas = faceapi.createCanvasFromMedia(webcam);
+        playCanvas.setAttribute('id', 'snapshot-canvas')
+        webcamContent.append(playCanvas)
     }
-    webcamContent.append(canvas)
+    webcamContent.append(playCanvas)
     marco.style.width =  webcam.offsetWidth + "px"
     marco.style.height =  webcam.offsetHeight + "px"
 
-    const displaySize = { width: webcam.offsetWidth, height: webcam.offsetHeight }
-    faceapi.matchDimensions(canvas, displaySize)
+    const displaySize = playDisplaySize = { width: webcam.offsetWidth, height: webcam.offsetHeight }
+    faceapi.matchDimensions(playCanvas, playDisplaySize)
 
     interval = setInterval(happinessFaceDetection(), 100)
 })
@@ -72,9 +75,9 @@ async function happinessFaceDetection()
         return
     }
     if (typeof detections[0].expressions != 'undefined') {
-        const resizedDetections = faceapi.resizeResults(detections, displaySize)
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        faceapi.draw.drawDetections(canvas, resizedDetections)
+        const resizedDetections = faceapi.resizeResults(detections, playDisplaySize)
+        playCanvas.getContext('2d').clearRect(0, 0, playCanvas.width, playCanvas.height)
+        faceapi.draw.drawDetections(playCanvas, resizedDetections)
         document.getElementById('nivelFelicidad').innerText = Math.floor((detections[0].expressions.happy) * 100) + '%'
 
         if (detections[0].expressions.happy >= 0.5) {
