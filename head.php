183557cc0b98a9cefec1a34ef2af42c88ac295a7
Jose Huancilla <jose@fragote.com>
2023-08-11 18:12:04 -0500

    update css cache

--- head.php
+++ head.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>Feliz Cumpleaños Angelina</title>
     <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-    <link href="assets/style.css" rel="stylesheet" crossorigin="anonymous">
+    <link href="assets/style.css?v=20230811161100" rel="stylesheet" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DynaPuff:wght@600;700&display=swap" rel="stylesheet">
