9762bc16bb31754b5d958182920f51a32795a4dd
Francis Gonzales <francis@fragote.com>
2025-08-21 14:37:25 -0500

    marco

--- image_manager.php
+++ image_manager.php
@@ -24,8 +24,8 @@ if (isset($_POST['imgB64'])) {
     imagecopyresampled($out, $jpeg, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
     imagecopyresampled($out, $png, 0, 0, 0, 0, $newwidth, $newheight, $newwidth, $newheight);
     imagejpeg($out, $path, 100);
+    imagecopy($out, $png, 0, 0, 0, 0, $newwidth, $newheight);
     
-
     echo $path;
 }
 
